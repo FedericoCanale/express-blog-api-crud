f1781abfa5f1cf2b30b5cdb98f2aadfb3a001608
FedericoCanale <icocanale@gmail.com>
2025-11-28 11:15:24 +0100

    register middleware into app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+const errorsHandler = require("./middlewares/errorsHandler");
 
 
+app.use(express.static("public"));
+app.use(express.json());
 
 app.use(express.static("public"));
 
